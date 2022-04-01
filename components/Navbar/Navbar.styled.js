7d7dae68e166d56153a0ec8d3e58f24fa538b5ef
benoithusson <benoit.husson@liip.ch>
2022-04-01 20:21:39 +0200

    Add StyledLink into generic component style

--- components/Navbar/Navbar.styled.js
+++ components/Navbar/Navbar.styled.js
@@ -51,34 +51,6 @@ export const MenuMobileIcon = styled.div`
   }
 `;
 
-export const StyledLink = styled.a`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin: 0 20px 0 20px;
-  background-color: none;
-  color: black;
-  text-decoration: none;
-  &::before {
-    content: "";
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 3px;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 128);
-    transform: scaleX(0);
-    transition: transform 0.3s ease;
-  }
-  &:hover::before {
-    transform: scaleX(1);
-  }
-`;
-
 export const Contacts = styled.span`
   width: 100%;
   background-color: green;
--- styles/generic-styled-components.js
+++ styles/generic-styled-components.js
@@ -4,3 +4,36 @@ export const ButtonMobile = styled.button`
   border: none;
   background-color: transparent;
 `;
+
+export const StyledLink = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0 20px 0 20px;
+  background-color: none;
+  color: black;
+  text-decoration: none;
+  &::before {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 3px;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 128);
+    transform: scaleX(0);
+    transition: transform 0.3s ease;
+  }
+  &:hover::before {
+    transform: scaleX(1);
+  }
+  @media (max-width: 768px) {
+    &::before {
+      display: none;
+    }
+  }
+`;
