559f1af9fd3b2ddb7e67b2d7a19244d395324020
benoithusson <benoit.husson@liip.ch>
2022-04-01 18:59:52 +0200

    Add ButtonMobile components + change size svg

--- components/Modal/Modal.js
+++ components/Modal/Modal.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import { ModalNavMobile } from "./Modal.styled";
-import svgCloseMenu from '../../assets/images/icon-close.svg';
+import { ButtonMobile } from "../../styles/generic-styled-components";
+import svgCloseMenuIcon from "../../assets/images/icon-close.svg";
 
 export default function Modal(props) {
   return (
     <ModalNavMobile stateNavMobile={props.stateNavMobile}>
-      <button onClick={() => props.changeStateNavMobile(!props.stateNavMobile)}>
-        <Image src={svgCloseMenu} width={20} height={20} layout="fixed" alt="Icon to close navigation on mobile" />
-      </button>
+      <ButtonMobile
+        onClick={() => props.changeStateNavMobile(!props.stateNavMobile)}
+      >
+        <Image
+          src={svgCloseMenuIcon}
+          width={40}
+          height={40}
+          layout="fixed"
+          alt="Icon to close navigation on mobile"
+        />
+      </ButtonMobile>
     </ModalNavMobile>
   );
 }
-
-{/* <a target="_blank" href="https://icons8.com/icon/46/close">Close</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,6 +2,9 @@
 // import burgerMenu from "../../assets/lotties/burger-menu.json";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import svgMenuIcon from "../../assets/images/icon-menu.svg";
+import { ButtonMobile } from "../../styles/generic-styled-components";
 import {
   Nav,
   BrandLogoContainer,
@@ -11,7 +14,6 @@ import {
 } from "./Navbar.styled";
 
 export default function Navbar(props) {
-
   return (
     <Nav>
       <BrandLogoContainer>Benoît</BrandLogoContainer>
@@ -36,7 +38,17 @@ export default function Navbar(props) {
         </Link>
       </NavItemsContainer>
       <MenuMobileIcon>
-        <button onClick={() => props.changeStateNavMobile(!props.stateNavMobile)}>Click</button>
+        <ButtonMobile
+          onClick={() => props.changeStateNavMobile(!props.stateNavMobile)}
+        >
+          <Image
+            src={svgMenuIcon}
+            width={40}
+            height={40}
+            layout="fixed"
+            alt="Icon to open menu navigation on mobile"
+          />
+        </ButtonMobile>
       </MenuMobileIcon>
     </Nav>
   );
