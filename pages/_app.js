10ad3cbb7d9b3b1b6fa93b6009e7716e37262a7f
benoithusson <benoit.husson@liip.ch>
2022-04-05 18:48:46 +0200

    Create context provider with state and setState

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import Container from "../components/Container";
 import GlobalStyle from "../styles/globalStyle";
+// Step 2) Import context into root element or parent element
+import { NavMobileContext } from "../helper/Context";
 
 function MyApp({ Component, pageProps }) {
   const [stateNavMobile, setStateNavMobile] = useState(false);
 
   return (
-    <Container
-      stateNavMobile={stateNavMobile}
-      setStateNavMobile={setStateNavMobile}
-    >
-      <GlobalStyle stateNavMobile={stateNavMobile} />
-      <Component {...pageProps} />
-    </Container>
+    // Step 3) Create the context provider + pass stateNavMobile and setState function
+    <NavMobileContext.Provider value={{stateNavMobile, setStateNavMobile}}>
+      <Container>
+        <GlobalStyle stateNavMobile={stateNavMobile} />
+        <Component {...pageProps} />
+      </Container>
+    </NavMobileContext.Provider>
   );
 }
 
