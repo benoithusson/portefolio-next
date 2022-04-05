647a3c23a43f3fb43184fafc4f8b22286d4f069d
benoithusson <benoit.husson@liip.ch>
2022-04-05 18:53:19 +0200

    Comments "food for thoughts"

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,17 @@ import { NavMobileContext } from "../helper/Context";
 function MyApp({ Component, pageProps }) {
   const [stateNavMobile, setStateNavMobile] = useState(false);
 
+  // When I click on hamburger icon, stateNavMobile changed
+  // So useContext works
+  // But why Modal is not displayed ?
+  console.log(stateNavMobile);
+
   return (
     // Step 3) Create the context provider + pass stateNavMobile and setState function
     <NavMobileContext.Provider value={{stateNavMobile, setStateNavMobile}}>
       <Container>
+        {/* How to use context into GlobalStyles ?
+        */}
         <GlobalStyle stateNavMobile={stateNavMobile} />
         <Component {...pageProps} />
       </Container>
